8b9ad2ed680c4c97b68a7bf9dd2cbd47d2310220
hoyoni <hoyeon.kim2773@gmail.com>
2025-09-05 08:04:19 +0900

    [Fix] change level 2 -> 4

--- pages/public/read-chapter.js
+++ pages/public/read-chapter.js
@@ -43,12 +43,17 @@ renderer.heading = (text, level) => {
     .toLowerCase()
     .replace(/[^\w]+/g, "-");
 
-  if (level === 2) {
-    return `<a name="${escapedText}" class="section-anchor" href="#${escapedText}">
-      <h${level} class="chapter-section">
-        ${text}
-      </h${level}>
-    </a>`;
+  if (level === 2 || level === 4) {
+    return `<a
+        style="color: #222"
+        class="section-anchor"
+        name="${escapedText}"
+        href="#${escapedText}"
+      >
+        <h${level} class="chapter-section">
+          ${text}
+        </h${level}>
+      </a>`;
   }
 
   return `<h${level}>${text}</h${level}>`;
