02485b13e4104eb43f01d8451a9a9bc4a55bf4bc
hoyoni <hoyeon.kim2773@gmail.com>
2025-12-09 20:11:45 +0900

    [Add] cacheKey has user id when req.user exists

--- server/routesWithSlug.js
+++ server/routesWithSlug.js
@@ -1,20 +1,69 @@
-export default function routesWithSlug({ server, app }) {
-  server.get("/books/:bookSlug/:chapterSlug", (req, res) => {
-    const { bookSlug, chapterSlug } = req.params;
-    app.render(req, res, "/public/read-chapter", { bookSlug, chapterSlug });
+import LRUCache from "lru-cache";
+
+export default function routesWithCache({ server, app }) {
+  const ssrCache = new LRUCache({
+    max: 100, // 100 items
+    maxAge: 1000 * 60 * 60, // 1hour
   });
 
-  server.get("/admin/edit-book/:slug", (req, res) => {
-    const { slug } = req.params;
-    app.render(req, res, "/admin/edit-book", { slug });
+  function getCacheKey(req) {
+    if (req.user) {
+      return `${req.url}${req.user.id}`;
+    }
+    return `${req.url}`;
+  }
+
+  async function renderAndCache(req, res, pagePath, queryParams) {
+    const key = getCacheKey(req);
+
+    // If we have a page in the cache, let's serve it
+    if (ssrCache.has(key)) {
+      res.setHeader("x-cache", "HIT");
+      res.send(ssrCache.get(key));
+      return;
+    }
+
+    try {
+      // If not let's render the page into HTML
+      const html = await app.renderToHTML(req, res, pagePath, queryParams);
+
+      // Something is wrong with the request, let's skip the cache
+      if (res.statusCode !== 200) {
+        res.send(html);
+        return;
+      }
+
+      // Let's cache this page
+      ssrCache.set(key, html);
+
+      res.setHeader("x-cache", "MISS");
+      res.send(html);
+    } catch (err) {
+      app.renderError(err, req, res, pagePath, queryParams);
+    }
+  }
+
+  server.get("/", (req, res) => {
+    renderAndCache(req, res, "/");
   });
 
-  server.get("/admin/book-detail/:slug", (req, res) => {
-    const { slug } = req.params;
-    app.render(req, res, "/admin/book-detail", { slug });
+  server.get("/book", (req, res) => {
+    renderAndCache(req, res, "/book");
   });
 
-  server.get("/books/:slug", (req, res) => {
-    res.redirect(`/books/${req.params.slug}/introduction`);
+  server.get("/book-reviews", (req, res) => {
+    renderAndCache(req, res, "/book-reviews");
+  });
+
+  server.get("/tutorials", (req, res) => {
+    renderAndCache(req, res, "/tutorials");
+  });
+
+  server.get("/login", (req, res) => {
+    renderAndCache(req, res, "/public/login");
+  });
+
+  server.get("/terms", (req, res) => {
+    renderAndCache(req, res, "/public/terms");
   });
 }
